cd5db149c244ad83cdb982c6f5284a54b2ad9d45
Christian <wiichanfaps@gmail.com>
2017-05-16 11:15:42 +0400

    change

--- mail.php
+++ mail.php
@@ -15,9 +15,35 @@ $message = "Yo!,
     End of message
     ";
 
-mail($myemail, $message);
+mail($myemail, $subject ,$message);
 
-header('location: index.html');
+header('location: thanks.html');
 
 exit();
+
+function check_input($data, $problem='')
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        if ($problem && strlen($data) ==0)
+        {
+            show_error($problem);
+        }
+        return $data;
+    }
+function show_error($myerror)
+    {
+        ?>
+            <html>
+            <body>
+            <b>Please correct the following error:</b><br />
+            <?php echo $myerror; ?>
+            </body>
+            </html>
+        <?php
+        exit();
+    }
+
+
 ?>  
